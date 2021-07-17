618628d4973e6ca0fc73e0a1729cf8feb2310f61
unknown <jhonrios@hotmail.es>
2021-07-16 19:38:25 -0500

    info

--- src/helper/auth.js
+++ src/helper/auth.js
@@ -9,19 +9,25 @@ Auth.verificartoken = (req, res, next) => {
 } */
 
 if (!req.headers.autorizacion) {
-    console.log(req.headers.autorizacion)
+    console.log("No estas autorizado token erroneo o en blanco")
+    /* console.log(req.headers.autorizacion) */
     return res.json({
     mensage: "No estas autorizado token erroneo o en blanco",
+
     });
+   
 }
 const token = req.headers.autorizacion.split(" ")[1];
+
 console.log(token)
 jwt.verify(token, "lol", (error, payload) => {
     if (error) {
+        console.log("No estas autorizado token caduco")
     return res.json({
         mensage: "Error de identificacion por token errado",
     });
     }
+    console.log("token valido")
     req.userid = payload._id;
     next();
 });
